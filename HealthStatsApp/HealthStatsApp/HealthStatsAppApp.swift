7bdcb9b54ab0205231196f935aa0a569a44f9e61
AnuGnane <89100965+AnuGnane@users.noreply.github.com>
2025-09-20 21:41:14 +0100

    Add files via upload

---
+++ HealthStatsApp/HealthStatsApp/HealthStatsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthStatsAppApp.swift
+//  HealthStatsApp
+//
+//  Created by DevTechie on 6/11/21.
+//
+
+import SwiftUI
+
+@main
+struct HealthStatsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
